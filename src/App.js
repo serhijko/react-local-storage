b85dfdb9fd8bef1dc38612c9e0cbe0c95652c98c
serhijko <serhijko>
2019-02-20 22:09:42 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { hits: null };
+  }
+
+  onSearch = (e) => {
+    e.preventDefault();
+
+    const { value } = this.input;
+
+    if (value === '') {
+      return;
+    }
+
+    const cachedHits = localStorage.getItem(value);
+    if (cachedHits) {
+      this.setState({ hits: JSON.parse(cachedHits) });
+      return;
+    }
+
+    fetch('https://hn.algolia.com/api/v1/search?query=' + value)
+      .then(response => response.json)
+      .then(result => this.onSetResult(result, value));
+  }
+
+  onSetResult = (result, key) => {
+    localStorage.setItem(key, JSON.stringify(result.hits));
+    this.setState({ hits: result.hits });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Search Hacker News with Local Storage</h1>
+        <p>
+          There shouldn't be a second network request,
+          when you search for something twice.
+        </p>
+
+        <form type="submit" onSubmit={this.onSearch}>
+          <input type="text" ref={node => this.input = node} />
+          <button type="button">Search</button>
+        </form>
+
+        {
+          this.state.hits &&
+          this.state.hits.map(item => <div key={item.objectID}>{item.title}</div>)
+        }
       </div>
     );
   }
